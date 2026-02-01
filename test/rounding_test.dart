fb008492c29006c28a2239315f01458f0a58fd2f
ContiHan <daniel.hanak@outlook.cz>
2026-02-01 20:56:28 +0100

    fixed issues

--- test/rounding_test.dart
+++ test/rounding_test.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/utils/formatter.dart';
+import 'package:crypto_miner_tycoon/utils/formatter.dart';
 
 void main() {
   test('Format Number Rounding Check', () {
